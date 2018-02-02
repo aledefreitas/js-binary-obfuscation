1bc8fc983ce7e0c4f3e079b5a5fd4f3ca6b30178
Alexandre de Freitas <alexandrefc2@hotmail.com>
2018-02-01 22:24:39 -0200

    Fix bin

--- bin/js-binary-obfuscation.js
+++ bin/js-binary-obfuscation.js
@@ -3,7 +3,7 @@
 (function() {
     var JSBinaryObfuscation = require('../index.js');
 
-    if(!proccess.argv[2] || fs.readFileSync(process.argv[2]))
+    if(!process.argv[2] || fs.readFileSync(process.argv[2]))
         return;
 
     new JSBinaryObfuscation(process.argv[2]);
